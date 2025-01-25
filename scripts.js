bfc5b1d7aa7f130e0f3a4fb3494e9fc42011b660
mk07080999 <mk07080999@gmail.com>
2025-01-25 18:05:02 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-   console.log("Image map example loaded.");
+```js
+function zoomIn() {
+    const img = document.getElementById("example-image");
+    const width = img.clientWidth;
+    img.style.width = (width * 1.2) + "px"; // 將寬度放大20%
+}
+
+function zoomOut() {
+    const img = document.getElementById("example-image");
+    const width = img.clientWidth;
+    img.style.width = (width * 0.8) + "px"; // 將寬度縮小20%
+}
